8c80ff863d6e16e4bdc63da7f0c59699054cca3e
lucas <lmino@fi.uba.ar>
2026-05-13 23:25:09 -0300

    corrijo funciones de ejecutar query que quedaron mal #2

--- backend/db/config.py
+++ backend/db/config.py
@@ -10,12 +10,12 @@ def get_connection():
 def ejecutar_query_lectura(query, params=None):
     with get_connection() as conn:
         with conn.cursor(dictionary=True) as cursor:
-            cursor.execute(query)
+            cursor.execute(query or ())
             return cursor.fetchall()
 
 def ejecutar_query_escritura(query, params=None):
     with get_connection() as conn:
         with conn.cursor(dictionary=True) as cursor:
-            cursor.execute(query)
+            cursor.execute(query or ())
             conn.commit() 
             return cursor.lastrowid
