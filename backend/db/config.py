b500277d71fcfc65d4d2f470f0a0033e0b24b85f
lucas <lmino@fi.uba.ar>
2026-05-13 22:42:14 -0300

    Agrego archivo config.py #2

---
+++ backend/db/config.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        database="restaurante"
+    )
+
+def ejecutar_query_lectura(query, params=None):
+    with get_connection() as conn:
+        with conn.cursor(dictionary=True) as cursor:
+            cursor.execute(query)
+            return cursor.fetchall()
+
+def ejecutar_query_escritura(query, params=None):
+    with get_connection() as conn:
+        with conn.cursor(dictionary=True) as cursor:
+            cursor.execute(query)
+            conn.commit() 
+            return cursor.lastrowid
