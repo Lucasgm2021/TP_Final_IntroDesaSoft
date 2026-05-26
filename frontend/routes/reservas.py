76c7108bc00bea9b16bc3fcc2c77c9a742361b07
MatiasJun <junmatias2001@gmail.com>
2026-05-26 00:14:32 -0300

    agregado de la vista home para admin #31

--- frontend/routes/reservas.py
+++ frontend/routes/reservas.py
@@ -37,3 +37,39 @@ def mis_reservas():
 @reserva_bp.route("/reservas_admin", methods=["GET"])
 def reservas_admin():
     return render_template("reservas_admin.html")
+
+@reserva_bp.route("/home_admin", methods=["GET"])
+def home_admin():
+
+    return render_template(
+        "home_admin.html",
+
+        mesas_ocupadas=12,
+        mesas_totales=30,
+
+        estacionamientos_ocupados=8,
+        estacionamientos_totales=20,
+
+        reservas_pendientes=[
+            {
+                "id_reserva": 1,
+                "estado_reserva": "pendiente",
+                "nombre_usuario": "Lionel Messi",
+                "comensales": 4,
+                "fecha": "2026-05-26",
+                "hora_reserva": "21:00:00",
+                "id_mesa": 12,
+                "interior": True
+            },
+            {
+                "id_reserva": 2,
+                "estado_reserva": "pendiente",
+                "nombre_usuario": "Maria Gomez",
+                "comensales": 2,
+                "fecha": "2026-05-26",
+                "hora_reserva": "22:00:00",
+                "id_mesa": 4,
+                "interior": False
+            }
+        ]
+    )
