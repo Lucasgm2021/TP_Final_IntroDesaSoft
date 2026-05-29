f500f6a3ccf2f4487b0b9361722b8907a96a7abf
Javier Aguirre <jaaguirre@fi.uba.ar>
2026-05-28 22:11:12 -0300

    menu - vistas publicas: agrego pagina /menu con grilla de platos

--- frontend/app.py
+++ frontend/app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 
 from routes.dashboard import dashboard_bp
 from routes.auth import auth_front_bp
+from routes.public import public_bp
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "mandarina"
 app.config["SESSION_PERMANENT"] = True
@@ -20,6 +21,7 @@ def inicio():
 
 app.register_blueprint(dashboard_bp, url_prefix="/dashboard")
 app.register_blueprint(auth_front_bp, url_prefix="/auth")
+app.register_blueprint(public_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
