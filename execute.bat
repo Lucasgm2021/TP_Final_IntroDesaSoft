f5f2f8ea1a5c3ff89b93e25c05d79cf53a312215
Lucas <lmino@fi.uba.ar>
2026-06-23 23:23:39 -0300

    correcciones de hora con docker y execute.bat

--- execute.bat
+++ execute.bat
@@ -18,7 +18,11 @@ cd ../frontend
 call .venv\Scripts\activate
 flask run --debug --port 5001
 
+pause >nul
+
 echo.
 echo Frontend detenido, cerrando backend...
-for /f "tokens=5" %%a in ('netstat -aon ^| findstr :5000') do taskkill /f /pid %%a 2>nul
+for /f "tokens=5" %%a in ('netstat -aon ^| findstr :5000') do taskkill /f /t /pid %%a 2>nul
+for /f "tokens=5" %%a in ('netstat -aon ^| findstr :5001') do taskkill /f /t /pid %%a 2>nul
+
 echo Backend detenido.
